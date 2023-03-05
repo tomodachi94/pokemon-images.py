ddf3faec541f4dbca285d6dd147b8962adcabf21
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-03-04 16:43:40 -0800

    ci: Init linting

---
+++ shell.nix
@@ -0,0 +1,8 @@
+with (import (fetchTarball https://github.com/nixos/nixpkgs/archive/037d4463a063ccdf4f257a7ee263033b0ba1820d.tar.gz) {});
+mkShell {
+  buildInputs = [
+    ruff
+    mypy
+  ];
+}
+
